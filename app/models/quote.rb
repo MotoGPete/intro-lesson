d660d804aec9292f2dca7068eeece8d5582960f6
Peter Boulton <peterboulton1228@gmail.com>
2019-09-01 00:34:01 +0000

    fixed error messages

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,4 @@
 class Quote < ApplicationRecord
+		validates :saying, presence: true, length: { maximum: 140, minimum: 3 }
+		validates :author, presence: true, length: { maximum: 50, minimum: 3 }, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
 end
